a80f2e05463b4f29cc82a846dd19965f765cd12a
mdhondi2020 <md.dhondi2020@gmail.com>
2021-04-03 11:35:12 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MDPipeline
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is for testing of pipeline");
+        }
+    }
+}
